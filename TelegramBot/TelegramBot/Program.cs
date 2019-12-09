12e48b629f2474cf9bfaaf7fe233a2e3c85a9fbd
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2019-12-09 10:38:44 +0500

    add

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -6,6 +6,7 @@ using System;
 using Telegram.Bot.Types.Enums;
 using System.IO;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineKeyboardButtons;
 
 namespace TelegramBot
 {
@@ -29,31 +30,49 @@ namespace TelegramBot
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             //file jonatishni boshlang'ichi
-            var stream = new FileStream(@"C:\Users\User\Desktop\1.jpg", FileMode.Open);  // faylni yuklab olindi          
-            var fileToSend = new FileToSend("asdasdas", stream); //filetosendga fileni tiqildi
+            //var stream = new FileStream(@"C:\Users\User\Desktop\1.jpg", FileMode.Open);  // faylni yuklab olindi          
+            //var fileToSend = new FileToSend("asdasdas", stream); //filetosendga fileni tiqildi
+            
             var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]// bu yerda location qabul qilish ishlatilvotdi
                         {
                             new KeyboardButton("Location") { RequestLocation = true } //keyboard bilan locationi qabul qilinvotdi
                         });
+            // key board yasash
+            var rkm = new ReplyKeyboardMarkup();
+            rkm.Keyboard =
+         new KeyboardButton[][]
+         {
+        new KeyboardButton[]
+        {
+            new KeyboardButton("item"),
+            new KeyboardButton("item")
+        },
+          new KeyboardButton[]
+        {
+            new KeyboardButton("item")
+        }
+         };
+            Bot.SendTextMessageAsync(e.Message.Chat.Id, "Text", ParseMode.Default, false, false, 0, rkm);
+            //inline yasash
+            var inlineKeyboard = new InlineKeyboardMarkup(new[] { new[] { InlineKeyboardButton.WithCallbackData("asdas", "1") } });
+            Bot.SendTextMessageAsync(e.Message.Chat.Id, "-->", replyMarkup: inlineKeyboard);
 
-            var result = Bot.SendTextMessageAsync(e.Message.Chat.Id, "Who or Where are you?", replyMarkup: RequestReplyKeyboard);
-            Console.WriteLine(e.Message.Location.Longitude+ " :Longitude   " + e.Message.Location.Latitude+ " :Latitude"); //locationni langetud va latetudesini consolga yozilvotdi
-            Bot.SendLocationAsync(e.Message.Chat.Id, e.Message.Location.Latitude, e.Message.Location.Longitude);
-            Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);// file jonatildi
+            
+            //var result = Bot.SendTextMessageAsync(e.Message.Chat.Id, "Who or Where are you?", replyMarkup: RequestReplyKeyboard);
+            //Console.WriteLine(e.Message.Location.Longitude+ " :Longitude   " + e.Message.Location.Latitude+ " :Latitude"); //locationni langetud va latetudesini consolga yozilvotdi
+
+            //Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);// file jonatildi
             //Console.WriteLine(e.Message.Date);
-
+            if (e.Message.Location != null)
+            {
+                Bot.SendLocationAsync(e.Message.Chat.Id, e.Message.Location.Latitude, e.Message.Location.Longitude);
+            }
             if (e.Message.Type==Telegram.Bot.Types.Enums.MessageType.TextMessage)
             {
-
-                
-                //Console.WriteLine(result.Result.Location);
-                
-
-                //Bot.SendDocumentAsync(e.Message.Chat.Id, file);
-                Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);                
+                //Bot.SendPhotoAsync(e.Message.Chat.Id, fileToSend);                
                 Console.WriteLine(e.Message.Chat.Id+"    "+e.Message.Text);
-                Bot.SendLocationAsync(e.Message.Chat.Id, 41.3196802f, 69.2428984f);
-                
+               
+
 
             }
             
