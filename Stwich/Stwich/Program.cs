7cb3b51fcd4193462bb1ee4f047ac60f3fc822b5
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2019-12-10 19:47:16 +0500

    фвв

--- Stwich/Stwich/Program.cs
+++ Stwich/Stwich/Program.cs
@@ -44,6 +44,26 @@ namespace Stwich
         //    public string Status { get; set; }      // статус пользователя
         //    public string Language { get; set; }
         //}
-        
+        //static void Main(string[] args)
+        //{
+        //    int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+        //    ref int numberRef = ref Find(4, numbers); // ищем число 4 в массиве
+        //    numberRef = 9; // заменяем 4 на 9
+        //    Console.WriteLine(numbers[3]); // 9
+
+        //    Console.Read();
+        //}
+        //static ref int Find(int number, int[] numbers)
+        //{
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        if (numbers[i] == number)
+        //        {
+        //            return ref numbers[i]; // возвращаем ссылку на адрес, а не само значение
+        //        }
+        //    }
+        //    throw new IndexOutOfRangeException("число не найдено");
+        //}
+
     }
 }
