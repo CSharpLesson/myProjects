d0f05822effeddc4454ad91cf6eb5768fc0b220c
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2019-12-10 19:32:38 +0500

    add

--- Stwich/Stwich/Program.cs
+++ Stwich/Stwich/Program.cs
@@ -19,6 +19,31 @@ namespace Stwich
                     Console.WriteLine("I don't know what say");
                     break;
             }
+            // switch pattern cvoystva bu faqat C# 8.0 ga
+            /*
+             * static int Select3(int op, int a, int b) => op switch
+        {
+            1 => a + b,
+            2 => a - b,
+            3 => a * b,
+            _ => throw new ArgumentException("Недопустимый код операции")
+        };
+             */
+
         }
+        //static string GetMessage(Person p) => p switch
+        //{
+        //    { Language: "english" } => "Hello!",
+        //    { Language: "german", Status: "admin" } => "Hallo!",
+        //    { Language: "french" } => "Salut!",
+        //    { } => "Hello world!"
+        //};
+        //class Person
+        //{
+        //    public string Name { get; set; }        // имя пользователя
+        //    public string Status { get; set; }      // статус пользователя
+        //    public string Language { get; set; }
+        //}
+        
     }
 }
