fd3df02ebf00d3529d31a74b3890c07c8f969108
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2019-12-28 15:22:06 +0500

    added

---
+++ WorkFile/WorkFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+using System.IO;
+
+namespace WorkFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Desktop";
+            DirectoryInfo drives = new  DirectoryInfo(path);
+            if (drives.Exists)
+            {
+                string a = @"C:\Users\User\Desktop\1.txt";
+                
+                string[] direct = Directory.GetFiles(path);
+                foreach (var dir in direct)
+                {
+                    WriteLine(dir);
+                    if (dir.EndsWith(".txt"))
+                    {
+                       
+                                               
+                    }
+                }
+
+            }
+            //foreach (var item in drives)
+            //{
+            //    //WriteLine(item.Name);
+            //    //WriteLine(item.DriveType);
+            //    //if (item.IsReady)
+            //    //{
+            //    //    WriteLine($"Объем диска: {item.TotalSize}");
+            //    //    WriteLine($"Свободное пространство: {item.TotalFreeSpace}");
+            //    //    WriteLine($"Метка: {item.VolumeLabel}");
+            //    //}
+            //}
+        }
+    }
+}
