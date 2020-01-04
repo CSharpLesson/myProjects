3bbcf229a5fead8934c54117302723da233b0212
Ibrohim avazov <ibrohim.avazov.98@mail.ru>
2020-01-04 16:26:36 +0500

    added

--- WorkFile/WorkFile/Program.cs
+++ WorkFile/WorkFile/Program.cs
@@ -9,21 +9,51 @@ namespace WorkFile
         {
             string path = @"C:\Users\User\Desktop";
             DirectoryInfo drives = new  DirectoryInfo(path);
-            if (drives.Exists)
+            if (!drives.Exists)
             {
-                string a = @"C:\Users\User\Desktop\1.txt";
-                
-                string[] direct = Directory.GetFiles(path);
-                foreach (var dir in direct)
-                {
-                    WriteLine(dir);
-                    if (dir.EndsWith(".txt"))
-                    {
-                       
-                                               
-                    }
-                }
+                drives.Create();
+                //string a = @"C:\Users\User\Desktop\1.txt";
 
+                //string[] direct = Directory.GetFiles(path);
+                //foreach (var dir in direct)
+                //{
+                //    WriteLine(dir);
+                //    if (dir.EndsWith(".txt"))
+                //    {                        
+                //    }//}            
+            }
+            string text = ReadLine();
+                
+                // запись в файл
+                using (FileStream file = new FileStream($"{path}\\note.txt", FileMode.OpenOrCreate))
+                {
+                    // преобразуем строку в байты
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    // запись массива байтов в файл
+                    file.Write(array, 0, array.Length);
+                    WriteLine("Текст записан в файл");
+                };
+            using (FileStream fstream = File.OpenRead($"{path}\\note.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                WriteLine($"Текст из файла: {textFromFile}");
+            }
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\User\Desktop\dd.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(text);
+            }
+
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\User\Desktop\dd.txt", true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine("Дозапись");
+                sw.Write(4.5);
             }
             //foreach (var item in drives)
             //{
--- primer/primer/Program.cs
+++ primer/primer/Program.cs
@@ -3,6 +3,7 @@ using System;
 using IronXL;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml;
 
 namespace primer
 {
@@ -10,17 +11,95 @@ namespace primer
     {
         static void Main(string[] args)
         {
-            List<User> users = new List<User>()
-{
-    new User { Name = "Tom", Age = 33 },
-    new User { Name = "Bob", Age = 30 },
-    new User { Name = "Tom", Age = 21 },
-    new User { Name = "Sam", Age = 43 }
-};
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(@"C:\Users\User\Desktop\offers Test.xml");
+            XmlElement xElem = xdoc.DocumentElement;
+            XmlNodeList childnodes = xElem.SelectNodes("//ПакетПредложений/Предложения/Предложение");
 
-            var result = users.OrderBy(u => u.Name).ThenBy(u => u.Age);
-            foreach (var item in result)
-                Console.WriteLine($"{item.Name} - {item.Age}");
+            foreach (XmlNode n in childnodes)
+            {
+                if (n.Name == "Предложение")
+                {
+                    foreach (XmlNode item1 in n.ChildNodes)
+                    {
+                        if (item1.Name == "Ид")
+                        {
+                            Console.WriteLine($"Ид :{item1.InnerText}");
+                        }
+                        if (item1.Name == "Штрихкод")
+                        {
+                            Console.WriteLine($"Штрихкод :{item1.InnerText}");
+                        }
+                        if (item1.Name == "Наименование")
+                        {
+                            Console.WriteLine($"Наименование :{item1.InnerText}");
+                        }
+                        if (item1.Name == "ЦенаЗаЕдиницу")
+                        {
+                            Console.WriteLine($"ЦенаЗаЕдиницу :{item1.InnerText}");
+                        }
+                        if (item1.Name == "Количество")
+                        {
+                            Console.WriteLine($"Количество :{item1.InnerText}");
+                        }
+                    }
+                }
+
+            }
+
+                //foreach (XmlNode xnode in xElem)
+                //{
+                //    foreach (XmlNode e in xnode.ChildNodes)
+                //    {
+                //        if (e.Name== "Предложения")
+                //        {
+                //            foreach (XmlNode item in e.ChildNodes)
+                //            {
+                //                if (item.Name== "Предложение")
+                //                {
+                //                    foreach (XmlNode item1 in item.ChildNodes)
+                //                    {
+                //                        if (item1.Name == "Ид")
+                //                        {
+                //                            Console.WriteLine($"Ид :{item1.InnerText}");
+                //                        }
+                //                        if (item1.Name == "Штрихкод")
+                //                        {
+                //                            Console.WriteLine($"Штрихкод :{item1.InnerText}");
+                //                        }
+                //                        if (item1.Name == "Наименование")
+                //                        {
+                //                            Console.WriteLine($"Наименование :{item1.InnerText}");
+                //                        }
+                //                        if (item1.Name == "ЦенаЗаЕдиницу")
+                //                        {
+                //                            Console.WriteLine($"ЦенаЗаЕдиницу :{item1.InnerText}");
+                //                        }
+                //                        if (item1.Name == "Количество")
+                //                        {
+                //                            Console.WriteLine($"Количество :{item1.InnerText}");
+                //                        }
+                //                    }
+                //                }
+                //            }
+
+                //        }
+                //    }
+                //}
+            
+            
+
+            //            List<User> users = new List<User>()
+            //{
+            //    new User { Name = "Tom", Age = 33 },
+            //    new User { Name = "Bob", Age = 30 },
+            //    new User { Name = "Tom", Age = 21 },
+            //    new User { Name = "Sam", Age = 43 }
+            //};
+
+            //            var result = users.OrderBy(u => u.Name).ThenBy(u => u.Age);
+            //            foreach (var item in result)
+            //                Console.WriteLine($"{item.Name} - {item.Age}");
             //Account<int> ac1 = new Account<int>(); //generic class
             //Account<string> ac2 = new Account<string>();
             //ac1.Id = 45;
@@ -55,48 +134,48 @@ namespace primer
             ////Save the excel file
             //xlsxWorkbook.SaveAs( $"{now.ToString("d")}.xlsx");
         }
-        static void Swap<T>(ref T x, ref T y)
-        {
-            T temp = x;
-            x = y;
-            y = temp;
-        }
-    }
-    class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public void Display()
-        {
-            Console.WriteLine(Name);
-        }
-    }
-    class Employ : Person
-    {
+        //    static void Swap<T>(ref T x, ref T y)
+        //    {
+        //        T temp = x;
+        //        x = y;
+        //        y = temp;
+        //    }
+        //}
+        //class Person
+        //{
+        //    public string Name { get; set; }
+        //    public int Age { get; set; }
+        //    public void Display()
+        //    {
+        //        Console.WriteLine(Name);
+        //    }
+        //}
+        //class Employ : Person
+        //{
 
-        public string Company { get; set; }
-        
-        public void Display()
-        {
-            base.Display();
-            Console.WriteLine($"{Name}   {Company}");
-        }
-    }
-    class Account<T>//generic klass
-    {
-        public T Id { get; set; }
-        public int Sum { get; set; }
-    }
-    class Trans<I, A>
-    {
-        public I From { get; set; }
-        public I To { get; set; }
-        public A Code { get; set; }
-    }
-    class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
+        //    public string Company { get; set; }
+
+        //    public void Display()
+        //    {
+        //        base.Display();
+        //        Console.WriteLine($"{Name}   {Company}");
+        //    }
+        //}
+        //class Account<T>//generic klass
+        //{
+        //    public T Id { get; set; }
+        //    public int Sum { get; set; }
+        //}
+        //class Trans<I, A>
+        //{
+        //    public I From { get; set; }
+        //    public I To { get; set; }
+        //    public A Code { get; set; }
+        //}
+        //class User
+        //{
+        //    public string Name { get; set; }
+        //    public int Age { get; set; }
 
     }
 }
