bc79b3c444d3ebf917b4f094957b726717b19740
Ibrohim <txsdasas@gmail.com>
2020-02-29 19:36:27 +0500

    added

--- Threads/Threads/Program.cs
+++ Threads/Threads/Program.cs
@@ -98,24 +98,21 @@ using System.Threading;
 //        }
 //    }
 //}
-//class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        int num = 0;
-//        // устанавливаем метод обратного вызова
-//        TimerCallback tm = new TimerCallback(Count);
-//        // создаем таймер
-//        Timer timer = new Timer(tm, num, 0, 2000);
-
-//        Console.ReadLine();
-//    }
-//    public static void Count(object obj)
-//    {
-//        int x = (int)obj;
-//        for (int i = 1; i < 9; i++, x++)
-//        {
-//            Console.WriteLine($"{x * i}");
-//        }
-//    }
-//}
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (false)
+        {
+            Console.WriteLine("+");
+        }
+    }
+    public static void Count(object obj)
+    {
+        int x = (int)obj;
+        for (int i = 1; i < 9; i++, x++)
+        {
+            Console.WriteLine($"{x * i}");
+        }
+    }
+}
